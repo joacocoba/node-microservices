cc7421356896132b70ce9a47424d9489d64038cd
joacocoba <jcaubarrere1@gmail.com>
2022-06-09 13:31:20 -0300

    Added Swagger Documentation

--- number-validator/src/app.js
+++ number-validator/src/app.js
@@ -2,6 +2,30 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const numberRoutes = require("./routes/numbers.routes");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Number-Validator",
+      description: "Un microservicio creado en Express.js",
+      contact: {
+        name: "Joaquin Caubarrere",
+      },
+      servers: [
+        {
+          url: "http:localhost:3000",
+        },
+      ],
+    },
+  },
+  apis: [`${path.join(__dirname, "./routes/*.js ")}`],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const publicDirectory = path.join(__dirname, "../public");
 app.use(express.json());
@@ -11,5 +35,7 @@ app.use(express.static(publicDirectory));
 require("./sockets/socket");
 
 app.use("/sendNumber", numberRoutes);
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
--- number-validator/src/routes/numbers.routes.js
+++ number-validator/src/routes/numbers.routes.js
@@ -2,6 +2,33 @@ const { Router } = require("express");
 const router = Router();
 const sendNumberController = require("../controllers/sendNumber.controller");
 
+/**
+ * @swagger
+ * /sendNumber:
+ *  post:
+ *      tags:
+ *          - "number"
+ *      summary: "Send a number to be validated"
+ *      consumes:
+ *          - "application/json"
+ *      produces:
+ *          - "application/json"
+ *      requestBody:
+ *         required: true
+ *         content:
+ *          application/json:
+ *              schema:
+ *                  type: object
+ *                  properties:
+ *                      number:
+ *                          type: number
+ *      description: "Endpoint used to send numbers, which will be validated and if they pass the validations they will be incorporated into the database."
+ *      responses:
+ *          "200":
+ *              description: "Worked well"
+ *          "400":
+ *              description: "Invalid input"
+ */
 router.post("/", sendNumberController);
 
 module.exports = router;
